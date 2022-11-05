1d5bed12d16ba389b66cb362452df288834a92f6
ellenbrianne <117481235+ellenbrianne@users.noreply.github.com>
2022-11-05 11:02:37 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var myAge = 26;
+
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+
+var laterYears = myAge -= 2;
+
+laterYears *= 4;
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+var myName = 'Ellen'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} in dog years.`);
+
